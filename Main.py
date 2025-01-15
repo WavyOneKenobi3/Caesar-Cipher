2e379a6f151f23a08bd1d058bb737d6ef9158b1d
Mark <19152780+WavyOneKenobi3@users.noreply.github.com>
2025-01-15 11:13:13 -0500

    main file update
    added alphabet file to program

---
+++ Main.py
@@ -0,0 +1,8 @@
+import alphabet
+
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrpyt:\n").lower()
+text = input("type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
